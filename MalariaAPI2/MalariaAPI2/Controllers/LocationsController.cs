11f8a3e6a1f9ff3f7ebf33f5e7bf1bceb44f6014
Rijnhardt008 <u17047057@tuks.co.za>
2019-04-28 09:16:49 +0200

    Location and Treatment API

---
+++ MalariaAPI2/MalariaAPI2/Controllers/LocationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MalariaAPI2.Models;
+using System.Web.Http.Cors;
+using System.Dynamic;
+
+namespace MalariaAPI2.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class LocationsController : ApiController
+    {
+        private Malaria_DBEntities db = new Malaria_DBEntities();
+
+        // GET: api/Locations
+        public List<dynamic> GetLocations()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Location> Loc1 = db.Locations.ToList();
+            List<dynamic> toReturn = new List<dynamic>();
+            foreach (Location c in Loc1)
+            {
+                dynamic m = new ExpandoObject();
+                m.Loc_ID = c.Loc_ID;
+                m.Loc_Des = c.Loc_Des;
+                m.Loc_Name = c.Loc_Name;
+                m.Loc_Severity = c.Loc_Severity;
+            }
+            return toReturn;
+        }
+
+        // GET: api/Locations/5
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult GetLocation(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(location);
+        }
+
+        // PUT: api/Locations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLocation(int id, Location location)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != location.Loc_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(location).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Locations
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult PostLocation(Location location)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Locations.Add(location);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = location.Loc_ID }, location);
+        }
+
+        // DELETE: api/Locations/5
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult DeleteLocation(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            db.Locations.Remove(location);
+            db.SaveChanges();
+
+            return Ok(location);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LocationExists(int id)
+        {
+            return db.Locations.Count(e => e.Loc_ID == id) > 0;
+        }
+    }
+}
---
+++ MalariaAPI2/MalariaAPI2/Controllers/TreatmentsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MalariaAPI2.Models;
+using System.Web.Http.Cors;
+using System.Dynamic;
+
+namespace MalariaAPI2.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class TreatmentsController : ApiController
+    {
+        private Malaria_DBEntities db = new Malaria_DBEntities();
+
+        // GET: api/Treatments
+        public List<dynamic> GetTreatments()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Treatment> Treat1 = db.Treatments.Include(zz => zz.Desease).ToList();
+            List<dynamic> toReturn = new List<dynamic>();
+            foreach (Treatment c in Treat1)
+            {
+                dynamic m = new ExpandoObject();
+                m.Treat_ID = c.Treat_ID;
+                m.Treat_Desc = c.Treat_Desc;
+                m.Treat_Type = c.Treat_Type;
+                m.Des_ID = c.Des_ID;
+                if (c.Desease != null)
+                {
+                    m.Des_Name = c.Desease.Des_Name;
+                }
+                toReturn.Add(m);
+            }
+            return toReturn;
+        }
+
+        // GET: api/Treatments/5
+        [ResponseType(typeof(Treatment))]
+        public IHttpActionResult GetTreatment(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            Treatment treatment = db.Treatments.Find(id);
+            if (treatment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(treatment);
+        }
+
+        // PUT: api/Treatments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTreatment(int id, Treatment treatment)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != treatment.Treat_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(treatment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TreatmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Treatments
+        [ResponseType(typeof(Treatment))]
+        public IHttpActionResult PostTreatment(Treatment treatment)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Treatments.Add(treatment);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = treatment.Treat_ID }, treatment);
+        }
+
+        // DELETE: api/Treatments/5
+        [ResponseType(typeof(Treatment))]
+        public IHttpActionResult DeleteTreatment(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            Treatment treatment = db.Treatments.Find(id);
+            if (treatment == null)
+            {
+                return NotFound();
+            }
+
+            db.Treatments.Remove(treatment);
+            db.SaveChanges();
+
+            return Ok(treatment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TreatmentExists(int id)
+        {
+            return db.Treatments.Count(e => e.Treat_ID == id) > 0;
+        }
+    }
+}
